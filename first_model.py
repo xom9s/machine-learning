7b434c1298c4c867c862e5d8f3eff1e8c00abf56
xom9s <xom9k321@gmail.com>
2024-08-17 18:52:59 +0300

    prediction on Kharkiv real estate prices

--- first_model.py
+++ first_model.py
@@ -1,6 +1,7 @@
+from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 X_train, X_test, y_train, y_test = train_test_split(
-    iris_dataset['data'], iris_dataset['target'], random_state=0)
+    iris_dataset['data'], iris_dataset['target'], random_state=0) # type: ignore
 
 knn = KNeighborsClassifier(n_neighbors=1)
 knn.fit(X_train, y_train)
